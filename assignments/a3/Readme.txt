d98f31ccb0a0986c68f5999ef646ae02e3ccb34d
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2025-03-18 22:02:22 -0600

    tweeks

---
+++ assignments/a3/Readme.txt
@@ -0,0 +1,9 @@
+
+# Assignment 3: Create a Panda-Chat
+#     CPSC 441 Winter 2025 | Benny Liang | 30192142
+
+---Compile/Running---
+Like any Python code, to run the server and client files in the terminal:
+`python <client/server file name>.py`
+(Note: should run the server file first then any client instances, ie more than one client)
+
--- assignments/a3/server_chat.py
+++ assignments/a3/server_chat.py
@@ -45,7 +45,7 @@ def is_unique_username(client_socket, username):
     LIST_OF_USERNAMES.append(username)
     joined_message = f"{username} joined the chat! {LIST_PANDA_THEMED_DECORATIONS[0]}{LIST_PANDA_THEMED_DECORATIONS[2]}" 
     send_message_to_chatroom(client_socket, username, joined_message) # announce a new user joined to all the clients
-
+    
     print(f"[SERVER] {username} joined the chat.")  # debug
     return "true" # is a unique username
 
@@ -80,6 +80,7 @@ def handle_client(client_socket, client_address):
                 break
                 
             print(f"received client message: {client_message}")
+            logging.info(f"Server received client message <{client_message}> from [{client_address}]")
             
             # incoming message will be in the form of (excluding the spaces): "<request type> | <user name> | <message>"
             request_type, username, message = client_message.strip().split("|")
@@ -89,18 +90,21 @@ def handle_client(client_socket, client_address):
                 leave_message = f"{username} has left..."
                 send_message_to_chatroom(client_socket, "Server", leave_message)        # notify the chat room this user left \
                 print(f"[SERVER] {username} has left.")
+                logging.info(f"[{client_address}] | {username} has left.")
                 break
             
             # send the `message` to the chat
             if (request_type == REQUEST_SEND_MESSAGE):
                 send_message_to_chatroom(client_socket, username, message)
                 print("[SERVER] sent message to all clients")     # debug
+                logging.info(f"[{client_address}] | sent message to chat: {message}")
                 
             # checking if the username entered is unique
             elif (request_type == REQUEST_CHECK_UNIQUE_USERNAME):
                 result = is_unique_username(client_socket, username).strip()
                 print(f"[SERVER] {username} is unique: {result}")  # debug
                 client_socket.send(result.encode())     # tell client the if the username is unique
+                logging.info(f"[{client_address}] | username <{message}> is unique: ")
                 
             elif (request_type == REQUEST_GROVE):
                 # formmated_list_of_connected_users = "@grove|----Connected users----\n"
@@ -111,6 +115,7 @@ def handle_client(client_socket, client_address):
                 
                 client_socket.send(formmated_list_of_connected_users.encode())
                 print("[SERVER] grove sent...")
+                logging.info(f"[{client_address}] | sent @grove {formmated_list_of_connected_users}.")
                 
             else:
                 print("_____INVALID REQUEST TYPE_____")
