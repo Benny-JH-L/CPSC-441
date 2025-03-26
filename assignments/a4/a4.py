e38f6cf68d37d7440729e4decc23df9abbb40ce6
Benny-JH-L <bennyliang24@gmail.com>
2025-03-26 17:44:18 -0600

    graph info parsing done

--- assignments/a4/a4.py
+++ assignments/a4/a4.py
@@ -7,6 +7,8 @@ import heapq
 from collections import defaultdict
 
 GRAPH_INFO_LOCATION = "magical_paths.txt" 
+DESTINATION_NODE = "Ottawa"
+infinity = 10e6
 
 def dijkstra(graph, start):
     """
@@ -84,12 +86,35 @@ if __name__ == "__main__":
     # create the graph
     # note: there are multiple magic path's from <start> to <dest> with diff edge info, so i store them as a list of lists,
     # for example: graph[start][destination] will have the edge values [[edge info 1], [edge info 2], ...], where [edge info #] contains <no. hops>, <distance (km)>, <time (hr)>, <dementor count>
-    graph = defaultdict(lambda: defaultdict(list))   # adjacency list representation
+    graph_uncondensed = defaultdict(lambda: defaultdict(list))   # adjacency list representation
     for edge in edges:
         start, dest, _, _, _, _ = edge
-        graph[start][dest].append(edge[2:])       # node `start` will point to node `dest` with `edge` info (removing the `start` and `dest` from `edge` list)
-        
-    print(graph)
+        graph_uncondensed[start][dest].append(edge[2:])       # node `start` will point to node `dest` with `edge` info (removing the `start` and `dest` from `edge` list)
+    # print(graph_uncondensed)    # debug
+    
+    # condense the graph (ie remove dup edges by taking the min values of each),
+    # for example, graph[start][destination] has values [[123, 123, 123, 4], [1, 123, 123, 123], [123, 2, 123, 123]]
+    # then the condensed version will be graph[start][destination] has values [1, 2, 123, 4]
+    graph = defaultdict(dict, {})
+    for key in graph_uncondensed.keys():
+        # print(f"{key}: {graph_uncondensed[key]}")   # debug
+        # loop through all edges
+        new_edge_info = [infinity, infinity, infinity, infinity] # -> [<no. hops>, <distance (km)>, <time (hr)>, <dementor count>]
+        for key2 in graph_uncondensed[key]: # get list of edges
+            # print(f"{key2}: {graph_uncondensed[key][key2]}") # debug
+            for edge in graph_uncondensed[key][key2]:   # loop through each edge
+                hop, distance, time, num_dementor = edge
+                # set the new edge info as the min values
+                new_edge_info = [min(new_edge_info[0], int(hop)), min(new_edge_info[1], int(distance)), min(new_edge_info[2], int(time)), min(new_edge_info[3], int(num_dementor))]
+            graph[key][key2] = new_edge_info
+    print(graph)    # debug
+    
+    harry_start = "British Columbia"
+    hermione_start = "Ontario"
+    ron_start = "Quebec"
+    luna_start = "Newfoundland and Labrador"
+    neville_start = "Saskatchewan"
+    ginny_start = "Nova Scotia"
     
     # # Example graph (adjacency list representation)
     # graph = defaultdict(dict, {
